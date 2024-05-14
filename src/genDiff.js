76bca1ab33fcee089baa361d0a72aa7c5c40da86
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-14 12:24:13 +0500

    add description and format option in genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,7 +2,9 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
   .option('-V, --version', 'output the version number')
+  .option('-f, --format  [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
