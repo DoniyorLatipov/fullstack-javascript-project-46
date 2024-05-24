f8ea543c1dfef2e315b4e28928586bb34ec95124
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-24 19:15:24 +0500

    refactor index.js: now return the difference between files (stylish)
    Now return the difference in the stylish format:
    
    {
      - follow: false,
        host: hexlet.io,
      + proxy: 123.234.53.22,
      - timeout: 50,
      + timeout: 20,
      ...
    }

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const parse = require('./src/parse.js');
+const _ = require('lodash');
 
 function difference(filepath1, filepath2) {
-  console.log(parse(filepath1));
-  console.log(parse(filepath2));
+  const data1 = parse(filepath1);
+  const data2 = parse(filepath2);
+  const res = [];
+
+  data1.forEach((line) => res.push(data2.includes(line) ? `  ${line}` : `- ${line}`));
+  data2.forEach((line) => res.push(data1.includes(line) ? `  ${line}` : `+ ${line}`));
+
+  const sortedRes = _.uniq(_.sortBy(res, (el) => el.slice(2, el.indexOf(':'))));
+  console.log(`{\n${' '.repeat(2)}${sortedRes.join(`,\n${' '.repeat(2)}`)}\n}\n`);
+  // for nested
 }
 
 module.exports = difference;
