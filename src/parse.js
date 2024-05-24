b309c7a0ea8c9c0adb5a1a6c8ad8b909f8713e38
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-24 19:11:03 +0500

    refactor parse.js: now return only lines with values
    now return lines without curly braces, quotation marks and commas

--- src/parse.js
+++ src/parse.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
 function parse(path) {
-  return fs.readFileSync(path, 'utf8', () => {});
+  return fs
+    .readFileSync(path, 'utf8', () => {})
+    .split(/\n|\r\n/)
+    .map((el) => el.trim().replaceAll(/,|"/g, ''))
+    .slice(1, -2);
 }
 
 module.exports = parse;
