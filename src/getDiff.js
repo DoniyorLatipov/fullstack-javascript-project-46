51133dfb9f63f1d0d6b0e8b00d4931facccfe4a1
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-29 19:52:04 +0500

    feat getDiff: returns the difference between two parsed data
    example:
    [
      [ ' ', 'host', 'hexlet.io' ],
      [ ' ', 'system', [ ['+', 'ip', '123.22'], [' ', 'os', 'Windows'] ] ],
      [ '-', 'timeout', 10 ],
      [ '+', 'timeout', 20 ]
    ]

---
+++ src/getDiff.js
@@ -0,0 +1,27 @@
+const _ = require('lodash');
+
+function getDiff(data1, data2) {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.union(keys1, keys2).sort();
+  const res = [];
+
+  keys.forEach((key) => {
+    if (data1[key] === data2[key]) {
+      res.push([' ', key, data1[key]]);
+    } else if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      res.push([' ', key, getDiff(data1[key], data2[key])]);
+    } else {
+      if (Object.hasOwn(data1, key)) {
+        res.push(['-', key, data1[key]]);
+      }
+      if (Object.hasOwn(data2, key)) {
+        res.push(['+', key, data2[key]]);
+      }
+    }
+  });
+
+  return res;
+}
+
+module.exports = getDiff;
