c8ea5493fd010f3d44fa78b85d307dfd446ce0ec
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-29 20:36:03 +0500

    feat render: outputs the diff according to the selected format
    so far there is only one format - “stylish”, which works by default
    
    render(diff, 'stylish') = render(diff)
    example:
    
    {
        main: {
          id: #23SID,
        + ip: 231.4
        },
      - mode: free,
      + os: Linux
    }

---
+++ src/render.js
@@ -0,0 +1,31 @@
+const _ = require('lodash');
+
+function stylishRender(data) {
+  if (data.length === 0) {
+    console.log('{}');
+  }
+
+  const iter = (data, depth) => {
+    const res = data.map(([symbol, key, value]) => {
+      return `${symbol} ${key}: ${_.isArray(value) ? iter(value, depth + 1) : value}`;
+    });
+
+    const space = ' '.repeat(depth * 2);
+    const endSpace = depth > 1 ? space : '';
+    return `{\n${space}${res.join(`,\n${space}`)}\n${endSpace}}`;
+  };
+
+  console.log(iter(data, 1));
+}
+
+function render(data, format) {
+  switch (format) {
+    case 'stylish':
+      stylishRender(data);
+      break;
+    default:
+      throw new Error(`unknown format, used ${format}`);
+  }
+}
+
+module.exports = render;
