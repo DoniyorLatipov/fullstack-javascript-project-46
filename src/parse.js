a178b38e433f18f90a30f38a9f8973de949b6ab3
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-29 19:38:30 +0500

    refactor parse: now parse using JSON "parse" method

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 
 function parse(path) {
-  return fs
-    .readFileSync(path, 'utf8', () => {})
-    .split(/\n|\r\n/)
-    .map((el) => el.trim().replaceAll(/,|"/g, ''))
-    .slice(1, -2);
+  return JSON.parse(fs.readFileSync(path, 'utf8', () => {}));
 }
 
 module.exports = parse;
