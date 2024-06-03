0d88106de985ecfa9e9292882fa10d124bb6f045
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-03 22:05:15 +0500

    fix getDiff(tests): fix work with nested data and tests
    - g: [object Object] => - g: {  some: none}

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -16,22 +16,122 @@ describe("getDiff's main flow", () => {
   });
 
   test('getDiff nested', () => {
-    const data1 = { host: 'hexlet.io', timeout: 10, system: { os: 'Linux', ip: '123.22' } };
-    const data2 = { timeout: 20, host: 'hexlet.io', system: { os: 'Windows', ip: '123.22' } };
+    const data1 = {
+      common: {
+        setting1: 'Value 1',
+        setting2: 200,
+        setting3: true,
+        setting6: {
+          key: 'value',
+          doge: {
+            wow: '',
+          },
+        },
+      },
+      group1: {
+        baz: 'bas',
+        foo: 'bar',
+        nest: {
+          key: 'value',
+        },
+      },
+      group2: {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+    };
+
+    const data2 = {
+      common: {
+        follow: false,
+        setting1: 'Value 1',
+        setting3: null,
+        setting4: 'blah blah',
+        setting5: {
+          key5: 'value5',
+        },
+        setting6: {
+          key: 'value',
+          ops: 'vops',
+          doge: {
+            wow: 'so much',
+          },
+        },
+      },
+      group1: {
+        foo: 'bar',
+        baz: 'bars',
+        nest: 'str',
+      },
+      group3: {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    };
 
     const expected = [
-      [' ', 'host', 'hexlet.io'],
       [
         ' ',
-        'system',
+        'common',
         [
-          [' ', 'ip', '123.22'],
-          ['-', 'os', 'Linux'],
-          ['+', 'os', 'Windows'],
+          ['+', 'follow', false],
+          [' ', 'setting1', 'Value 1'],
+          ['-', 'setting2', 200],
+          ['-', 'setting3', true],
+          ['+', 'setting3', null],
+          ['+', 'setting4', 'blah blah'],
+          ['+', 'setting5', [[' ', 'key5', 'value5']]],
+          [
+            ' ',
+            'setting6',
+            [
+              [
+                ' ',
+                'doge',
+                [
+                  ['-', 'wow', ''],
+                  ['+', 'wow', 'so much'],
+                ],
+              ],
+              [' ', 'key', 'value'],
+              ['+', 'ops', 'vops'],
+            ],
+          ],
         ],
       ],
-      ['-', 'timeout', 10],
-      ['+', 'timeout', 20],
+      [
+        ' ',
+        'group1',
+        [
+          ['-', 'baz', 'bas'],
+          ['+', 'baz', 'bars'],
+          [' ', 'foo', 'bar'],
+          ['-', 'nest', [[' ', 'key', 'value']]],
+          ['+', 'nest', 'str'],
+        ],
+      ],
+      [
+        '-',
+        'group2',
+        [
+          [' ', 'abc', 12345],
+          [' ', 'deep', [[' ', 'id', 45]]],
+        ],
+      ],
+      [
+        '+',
+        'group3',
+        [
+          [' ', 'deep', [[' ', 'id', [[' ', 'number', 45]]]]],
+          [' ', 'fee', 100500],
+        ],
+      ],
     ];
     expect(getDiff(data1, data2)).toStrictEqual(expected);
   });
--- src/getDiff.js
+++ src/getDiff.js
@@ -7,16 +7,18 @@ export default function getDiff(data1, data2) {
   const res = [];
 
   keys.forEach((key) => {
-    if (data1[key] === data2[key]) {
-      res.push([' ', key, data1[key]]);
-    } else if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       res.push([' ', key, getDiff(data1[key], data2[key])]);
+    } else if (data1[key] === data2[key]) {
+      res.push([' ', key, data1[key]]);
     } else {
       if (Object.hasOwn(data1, key)) {
-        res.push(['-', key, data1[key]]);
+        const value1 = _.isObject(data1[key]) ? getDiff(data1[key], data1[key]) : data1[key];
+        res.push(['-', key, value1]);
       }
       if (Object.hasOwn(data2, key)) {
-        res.push(['+', key, data2[key]]);
+        const value2 = _.isObject(data2[key]) ? getDiff(data2[key], data2[key]) : data2[key];
+        res.push(['+', key, value2]);
       }
     }
   });
