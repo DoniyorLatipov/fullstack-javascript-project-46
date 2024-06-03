5691faab1bdb434bc96a6c36dba9aedfb919b812
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-03 20:19:42 +0500

    feat parse: now parse YAML files

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,23 @@
 import fs from 'fs';
+import { extname } from 'path';
+import yaml from 'js-yaml';
 
 export default function parse(path) {
-  return JSON.parse(fs.readFileSync(path, 'utf8', () => {}));
+  const format = extname(path);
+
+  let formatParse;
+  switch (format) {
+    case '.json':
+      formatParse = JSON.parse;
+      break;
+    case '.yml':
+    case '.yaml':
+      formatParse = yaml.load;
+      break;
+    default:
+      throw new Error(`Unexpected file format, open ${path} (${format})`);
+  }
+
+  const data = fs.readFileSync(path, 'utf8', () => {});
+  return formatParse(data);
 }
