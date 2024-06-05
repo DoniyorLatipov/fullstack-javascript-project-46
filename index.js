2abb1179236b1ddff07232a32e392daadcf93ff2
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-05 14:00:56 +0500

    refactor diffFormat: now returns formatted differenc
    Before this, the rendering function was used, which formatted the
    difference to the selected format, now the difference is generated
    to the selected format

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import parse from './src/parse.js';
 import getDiff from './src/getDiff.js';
-import render from './src/render.js';
 
 export default function difference(filepath1, filepath2, { format }) {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
 
-  const difference = getDiff(data1, data2);
-  render(difference, format);
+  const difference = getDiff(data1, data2, format);
+  console.log(difference);
 }
--- src/getDiff.js
+++ src/getDiff.js
@@ -1,27 +1,13 @@
-import _ from 'lodash';
+import stylishDiff from './diffFormat/stylish.js';
+import plainDiff from './diffFormat/plain.js';
 
-export default function getDiff(data1, data2) {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.union(keys1, keys2).sort();
-  const res = [];
-
-  keys.forEach((key) => {
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      res.push([' ', key, getDiff(data1[key], data2[key])]);
-    } else if (data1[key] === data2[key]) {
-      res.push([' ', key, data1[key]]);
-    } else {
-      if (Object.hasOwn(data1, key)) {
-        const value1 = _.isObject(data1[key]) ? getDiff(data1[key], data1[key]) : data1[key];
-        res.push(['-', key, value1]);
-      }
-      if (Object.hasOwn(data2, key)) {
-        const value2 = _.isObject(data2[key]) ? getDiff(data2[key], data2[key]) : data2[key];
-        res.push(['+', key, value2]);
-      }
-    }
-  });
-
-  return res;
+export default function getDiff(data1, data2, format) {
+  switch (format) {
+    case 'stylish':
+      return stylishDiff(data1, data2);
+    case 'plain':
+      return plainDiff(data1, data2);
+    default:
+      throw new Error(`unknown format, used ${format}`);
+  }
 }
