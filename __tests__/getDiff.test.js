b52eee4046c6de36bf40814207500aa76e48bde1
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-05 15:12:31 +0500

    style getDiff.test: rename special expect constant for each case
    expected = ... => expect<Format><Type>
    
    <Format>  Stylish/Plain
    <Type>    Plane/Nested

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -7,20 +7,20 @@ describe("getDiff's main flow", () => {
     const data2 = { timeout: 20, verbose: true, host: 'hexlet.io' };
 
     test('stylish format', () => {
-      const expected = `{
+      const expectedStylishPlane = `{
     host: hexlet.io
   - timeout: 10
   + timeout: 20
   + verbose: true
 }`;
-      expect(getDiff(data1, data2, 'stylish')).toBe(expected);
+      expect(getDiff(data1, data2, 'stylish')).toBe(expectedStylishPlane);
     });
 
     test('plain format', () => {
-      const expected = `Property 'timeout' was updated. From 10 to 20
+      const expectedPlainPlane = `Property 'timeout' was updated. From 10 to 20
 Property 'verbose' was added with value: true`;
 
-      expect(getDiff(data1, data2, 'plain')).toBe(expected);
+      expect(getDiff(data1, data2, 'plain')).toBe(expectedPlainPlane);
     });
   });
 
@@ -85,7 +85,7 @@ Property 'verbose' was added with value: true`;
     };
 
     test('stylish format', () => {
-      const expected = `{
+      const expectedStylishNested = `{
     common: {
       + follow: false
         setting1: Value 1
@@ -129,11 +129,11 @@ Property 'verbose' was added with value: true`;
         fee: 100500
     }
 }`;
-      expect(getDiff(data1, data2, 'stylish')).toBe(expected);
+      expect(getDiff(data1, data2, 'stylish')).toBe(expectedStylishNested);
     });
 
     test('plain format', () => {
-      const expected = `Property 'common.follow' was added with value: false
+      const expectedPlainNested = `Property 'common.follow' was added with value: false
 Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
@@ -144,7 +144,7 @@ Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
-      expect(getDiff(data1, data2, 'plain')).toBe(expected);
+      expect(getDiff(data1, data2, 'plain')).toBe(expectedPlainNested);
     });
   });
 });
