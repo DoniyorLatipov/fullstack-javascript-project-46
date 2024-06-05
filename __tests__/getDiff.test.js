60fac58a88414efddeb0610e0158107a5333977a
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-05 14:18:37 +0500

    refactor tests(getDiff): configure getDiff's tests for new formatting

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -2,20 +2,29 @@
 import getDiff from '../src/getDiff.js';
 
 describe("getDiff's main flow", () => {
-  test('getDiff plane', () => {
+  describe('getDiff plane', () => {
     const data1 = { host: 'hexlet.io', timeout: 10 };
     const data2 = { timeout: 20, verbose: true, host: 'hexlet.io' };
 
-    const expected = [
-      [' ', 'host', 'hexlet.io'],
-      ['-', 'timeout', 10],
-      ['+', 'timeout', 20],
-      ['+', 'verbose', true],
-    ];
-    expect(getDiff(data1, data2)).toStrictEqual(expected);
+    test('stylish format', () => {
+      const expected = `{
+    host: hexlet.io
+  - timeout: 10
+  + timeout: 20
+  + verbose: true
+}`;
+      expect(getDiff(data1, data2, 'stylish')).toBe(expected);
+    });
+
+    test('plain format', () => {
+      const expected = `Property 'timeout' was updated. From 10 to 20
+Property 'verbose' was added with value: true`;
+
+      expect(getDiff(data1, data2, 'plain')).toBe(expected);
+    });
   });
 
-  test('getDiff nested', () => {
+  describe('getDiff nested', () => {
     const data1 = {
       common: {
         setting1: 'Value 1',
@@ -75,64 +84,67 @@ describe("getDiff's main flow", () => {
       },
     };
 
-    const expected = [
-      [
-        ' ',
-        'common',
-        [
-          ['+', 'follow', false],
-          [' ', 'setting1', 'Value 1'],
-          ['-', 'setting2', 200],
-          ['-', 'setting3', true],
-          ['+', 'setting3', null],
-          ['+', 'setting4', 'blah blah'],
-          ['+', 'setting5', [[' ', 'key5', 'value5']]],
-          [
-            ' ',
-            'setting6',
-            [
-              [
-                ' ',
-                'doge',
-                [
-                  ['-', 'wow', ''],
-                  ['+', 'wow', 'so much'],
-                ],
-              ],
-              [' ', 'key', 'value'],
-              ['+', 'ops', 'vops'],
-            ],
-          ],
-        ],
-      ],
-      [
-        ' ',
-        'group1',
-        [
-          ['-', 'baz', 'bas'],
-          ['+', 'baz', 'bars'],
-          [' ', 'foo', 'bar'],
-          ['-', 'nest', [[' ', 'key', 'value']]],
-          ['+', 'nest', 'str'],
-        ],
-      ],
-      [
-        '-',
-        'group2',
-        [
-          [' ', 'abc', 12345],
-          [' ', 'deep', [[' ', 'id', 45]]],
-        ],
-      ],
-      [
-        '+',
-        'group3',
-        [
-          [' ', 'deep', [[' ', 'id', [[' ', 'number', 45]]]]],
-          [' ', 'fee', 100500],
-        ],
-      ],
-    ];
-    expect(getDiff(data1, data2)).toStrictEqual(expected);
+    test('stylish format', () => {
+      const expected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+      expect(getDiff(data1, data2, 'stylish')).toBe(expected);
+    });
+
+    test('plain format', () => {
+      const expected = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
+      expect(getDiff(data1, data2, 'plain')).toBe(expected);
+    });
   });
 });
