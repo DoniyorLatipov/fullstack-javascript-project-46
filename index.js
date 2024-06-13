a90122a5671f528b1a5f2150cb3ff07a422f2dcd
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-13 14:53:27 +0500

    Revert all commits after merge refactor-formats
    This reverts commit d38d4ff9ca3bcb161ad89c0750ebc3a38fc9f89e.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import parse from './src/parse.js';
 import getDiff from './src/formatters/index.js';
 
-export default function difference(filepath1, filepath2, { format } = {}) {
+export default function difference(filepath1, filepath2, { format }) {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
 
-  const difference = getDiff(data1, data2, format ?? 'stylish');
+  const difference = getDiff(data1, data2, format);
   console.log(difference);
-  return difference;
 }
