8d10204015677378b91a4e0186382f66ed093393
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-13 14:28:19 +0500

    refactor index.js: use ?? in getDiff call for def value

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ export default function difference(filepath1, filepath2, { format } = {}) {
   const data1 = parse(filepath1);
   const data2 = parse(filepath2);
 
-  const difference = getDiff(data1, data2, format);
+  const difference = getDiff(data1, data2, format ?? 'stylish');
   console.log(difference);
 }
