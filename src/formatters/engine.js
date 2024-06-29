bc41dc5d34cc1e5a8bf8af9fa45403342d4246c7
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-06-29 10:30:07 +0500

    style: rename repeating variables in the function body (iter)

--- src/formatters/engine.js
+++ src/formatters/engine.js
@@ -1,39 +1,29 @@
-import getKeys from '../getKeys.js';
 import _ from 'lodash';
+import getKeys from '../getKeys.js';
 
 export default function formatterEngine(
   data1,
   data2,
-  {
-    iterValue,
-    getDefaultAcc,
-    getNewIterValue,
-    merge,
-    addChanged,
-    addAdded,
-    addRemoved,
-    addUnchanged,
-    convert,
-  },
+  { iterValue, getDefaultAcc, getNewIterValue, merge, addChanged, addAdded, addRemoved, addUnchanged, convert },
 ) {
-  const iter = (data1, data2, iterValue) => {
-    const keys = getKeys(data1, data2);
+  const iter = (obj1, obj2, initialIterValue) => {
+    const keys = getKeys(obj1, obj2);
 
     return keys.reduce((acc, key) => {
-      const value1 = data1[key];
-      const value2 = data2[key];
+      const value1 = obj1[key];
+      const value2 = obj2[key];
 
       if (_.isObject(value1) && _.isObject(value2)) {
-        const newIterValue = getNewIterValue(iterValue, key);
+        const newIterValue = getNewIterValue(initialIterValue, key);
         acc = merge(acc, iter(value1, value2, newIterValue), key);
-      } else if (!Object.hasOwn(data1, key)) {
-        acc = addAdded(acc, key, value2, iterValue);
-      } else if (!Object.hasOwn(data2, key)) {
-        acc = addRemoved(acc, key, value1, iterValue);
+      } else if (!Object.hasOwn(obj1, key)) {
+        acc = addAdded(acc, key, value2, initialIterValue);
+      } else if (!Object.hasOwn(obj2, key)) {
+        acc = addRemoved(acc, key, value1, initialIterValue);
       } else if (value1 !== value2) {
-        acc = addChanged(acc, key, [value1, value2], iterValue);
+        acc = addChanged(acc, key, [value1, value2], initialIterValue);
       } else {
-        acc = addUnchanged(acc, key, value1, iterValue);
+        acc = addUnchanged(acc, key, value1, initialIterValue);
       }
       return acc;
     }, getDefaultAcc());
